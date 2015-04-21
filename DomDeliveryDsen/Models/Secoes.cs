f78ce0e0f246ef115eea21d6136a2520c95a8648
fparanha <felipe@tk95.com.br>
2015-04-21 15:41:09 -0300

    Classe Secoes

---
+++ DomDeliveryDsen/Models/Secoes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DomDeliveryDsen.Models
+{
+    public class Secoes
+    {
+        public virtual int Id { get; set; }
+        public virtual Loja IdLoja { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual bool Exibir { get; set; }
+        public virtual int OrdemExibicao { get; set; }
+        public virtual bool SttObservacao { get; set; }
+        public virtual bool SttAdicional { get; set; }
+        public virtual bool SttProdutosPromocionais { get; set; }
+        public virtual bool SttTamanho { get; set; }
+        public virtual bool SttPizza { get; set; }
+        public virtual bool SttObsPizza { get; set; }
+    }
+}
