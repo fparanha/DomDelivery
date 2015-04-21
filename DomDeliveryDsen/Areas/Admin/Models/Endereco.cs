aeb91ce6d1f2afff22b529ecdbb71fba1e5eb1d2
fparanha <felipe@tk95.com.br>
2015-04-21 17:24:56 -0300

    Classe Endereco

---
+++ DomDeliveryDsen/Areas/Admin/Models/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DomDeliveryDsen.Areas.Admin.Models
+{
+    public class Endereco
+    {
+        public virtual int IdCep { get; set; }
+        public virtual string Cep { get; set; }
+        public virtual string Logradouro { get; set; }
+        public virtual int Numero { get; set; }
+        public virtual string Complemento { get; set; }
+        public virtual string Bairro { get; set; }
+        public virtual string Cidade { get; set; }
+        public virtual string Uf { get; set; }
+
+    }
+}
