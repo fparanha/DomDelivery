ea7ee81710518eacf54ce8d0f9c2d8e2e3bd3275
fparanha <felipe@tk95.com.br>
2015-04-21 14:43:21 -0300

    Classe Empresa

--- DomDeliveryDsen/Models/Empresa.cs
+++ DomDeliveryDsen/Models/Empresa.cs
@@ -7,5 +7,10 @@ namespace DomDeliveryDsen.Models
 {
     public class Empresa
     {
+        public virtual int IdEmpresa { get; set; }
+        public virtual string NomeEmpresa { get; set; }
+        public virtual string RazaoSocial { get; set; }
+        public virtual string Url { get; set; }
+        public virtual string Pasta { get; set; }        
     }
 }
