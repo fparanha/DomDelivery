bc64121495e115a981eb03cc9b907d3c326677ef
fparanha <felipe@tk95.com.br>
2015-04-21 15:30:15 -0300

    Classe Tamanho

---
+++ DomDeliveryDsen/Models/Tamanho.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DomDeliveryDsen.Models
+{
+    public class Tamanho
+    {
+        public virtual int Id { get; set; }
+        public virtual Loja IdLoja { get; set; }
+        public virtual string Nome { get; set; }
+        public virtual string Descricao { get; set; }
+        public virtual int QuantidadeSabor { get; set; }
+        public virtual int QuantidadeFatias { get; set; }
+        public virtual bool SttPromocao { get; set; }
+    }
+}
